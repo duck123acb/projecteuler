887fd2e3fe36cfe276e3494d8ca95751b3e676e8
Danush Ramanan <dan.ramanan@gmail.com>
2025-06-25 22:00:10 -0600

    ciommit quickly

---
+++ problems/5.py
@@ -0,0 +1,29 @@
+# https://projecteuler.net/problem=5
+
+from utils import check
+
+def is_divisible(n):
+  for m in range 
+  11
+  12
+  13
+  14
+  15
+  16
+  17
+  18
+  19
+  20
+
+def main():
+  final = 0
+  
+  # solve here ig
+
+  return final
+
+problem = 5
+answer = main()
+
+print("https://projecteuler.net/problem=" + str(problem))
+check(answer, problem)
